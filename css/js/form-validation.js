386a4f3f607d4e0f1923a9a5eb1288aa8eb0d428
Matthew Burns <147122838+mburns00@users.noreply.github.com>
2024-08-16 22:43:21 +1000

    Delete css directory
    Uploading to github seems to be breaking my media files, jsut going to revert back to version 1.

--- css/js/form-validation.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    // Basic form validation can be added here
-    alert("Form submitted!");
-});
